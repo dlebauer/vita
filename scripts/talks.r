b63d3e1b13096de6d72006010f7a397849bd9bda
James Keirstead <james.keirstead@gmail.com>
2015-02-26 18:39:21 +0000

    Converted talks to a YAML file with R parser

---
+++ scripts/talks.r
@@ -0,0 +1,18 @@
+## Test for parsing YAML
+
+library(yaml)
+
+## Load the talks
+talks <- yaml.load_file("../content/talks.yaml")[[1]]
+
+## Sort by year order
+ord <- order(unlist(lapply(talks, function(x) x$year)))
+talks <- talks[ord]
+
+lines <- lapply(talks, function(x) {
+    with(x, sprintf("\\ind %d ``%s''. %s, %s. %s.\n", year, title, event, city, month))
+})
+
+out.file <- "../templates/talks.tex"
+file.remove(out.file)
+invisible(lapply(lines, write, file=out.file, append=TRUE))
