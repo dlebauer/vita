3c5fe5088305a9e60908b11c83ee41648dedf416
James Keirstead <james.keirstead@gmail.com>
2015-02-27 20:29:05 +0000

    Refactored process_yaml so it now has a simpler API

--- scripts/all_sections.r
+++ scripts/all_sections.r
@@ -1,39 +1,12 @@
 source("yaml.r")
-source("tex_functions.r")
 
-var <- "affiliations"
-process_yaml("../content/affiliations.yaml", 
-             "../templates/affiliations.tex",
-             paste0("format_", var))
-
-process_yaml("../content/education.yaml",
-             "../templates/education.tex",
-             "format_education")
-
-process_yaml("../content/grants.yaml",
-             "../templates/grants.tex",
-             "format_grants")
-
-process_yaml("../content/awards.yaml",
-             "../templates/awards.tex",
-             "format_awards")
-
-process_yaml("../content/posts.yaml",
-             "../templates/posts.tex",
-             "format_posts")
-
-process_yaml("../content/service.yaml",
-             "../templates/service.tex",
-             "format_service")
-
-process_yaml("../content/students.yaml",
-             "../templates/students.tex",
-             "format_students")
-
-process_yaml("../content/talks.yaml",
-             "../templates/talks.tex",
-             "format_talks")
-
-process_yaml("../content/teaching.yaml",
-             "../templates/teaching.tex",
-             "format_teaching")
+sections <- c("affiliations",
+              "education",
+              "grants",
+              "awards",
+              "posts",
+              "service",
+              "students",
+              "talks",
+              "teaching")
+invisible(lapply(sections, function(s) process_yaml(s, format="tex")))
--- scripts/yaml.r
+++ scripts/yaml.r
@@ -4,21 +4,30 @@ library(yaml)
 
 #' Processes a specified YAML file to create outputs
 #'
-#' @param input a character giving the YAML file
-#' @param output a character giving the output file name
-#' @param f a character giving the name of the function describing how
-#' to parse the YAML file.  It should take a list of YAML data and
-#' process appropriately, returning a list of output lines
-#'
+#' @param section a character giving the name of the section to
+#' process.  There must be a corresponding \code{section.yaml} file in
+#' the input directory and \code{format_section} function in the
+#' formats file.  The function should take a YAML parsed list as input
+#' and returned a list of lines in the correct format.
+#' @param input_dir the input directory (no trailing /)
+#' @param output_dir the output directory (no trailing /)
+#' @param format a character giving a file name of the format
+#' \code{format_functions.r}
+#' 
 #' @return NULL
-process_yaml <- function(input, output, f) {
+process_yaml <- function(section, input_dir="../content", output_dir="../templates", format) {
 
+    input <- file.path(input_dir, paste0(section, ".yaml"))
+    output <- file.path(output_dir, paste0(section, ".", format))
+    source(paste0(format, "_functions.r"))
+    
     if (!file.exists(input)) stop(sprintf("Unable to find input file '%s'", input))
 
     message(sprintf("Processing '%s'...", input), appendLF=FALSE)
     
     data <- yaml.load_file(input)
 
+    f <- paste0("format_", section)
     lines <- eval(call(f, data))
 
     if (file.exists(output)) file.remove(output)
