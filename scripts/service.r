c2cf1ff0de32bd02731b1e4678a4ac467b2f7db8
James Keirstead <james.keirstead@gmail.com>
2015-02-27 19:39:14 +0000

    Converted the service file

--- scripts/service.r
+++ scripts/service.r
@@ -1,36 +1,41 @@
 ## Test for parsing YAML
-
-library(yaml)
-
-## Load the data
-data <- yaml.load_file("../content/service.yaml")
-
-## First process the committees etc
-## Sort by year order
-ord <- order(unlist(lapply(data[[1]], function(x) x$start)))
-comms <- data[[1]][ord]
-
-comm.lines <- lapply(comms, function(x) {
-    if ("end" %in% names(x)) {
-        with(x, sprintf("\\ind %d--%s.  %s, %s.\n", start, end, role, entity))
-    } else {
-        with(x, sprintf("\\ind %d.  %s, %s.\n", start, role, entity))
-    }
-})
-
-## Then grant reviews
-grants <- paste0("\\ind Research proposal reviewer for ", paste0(data[[2]], collapse=", "), "\n")
-
-## Then journal reviews
-journals <- paste0("\\ind Journal reviewer for ",
-                   paste0(paste("\\emph{", data[[3]], "}", sep=""), collapse=", "), "\n")
+source("yaml.r")
 library(stringr)
-journals <- str_replace(journals, "&", "\\\\&")
 
-## Then consultancy
-consultancy <- paste0("\\ind Independent consultancy for ", paste0(data[[4]], collapse=", "), "\n")
+f <- function(l) {
 
-lines <- c(comm.lines, grants, journals, consultancy)
-out.file <- "../templates/service.tex"
-if (file.exists(out.file)) file.remove(out.file)
-invisible(lapply(lines, write, file=out.file, append=TRUE))
+    tmp <- l[[1]]
+
+    ## First process the committees etc
+    ## Sort by year order
+    ord <- order(unlist(lapply(tmp, function(x) x$start)))
+    comms <- tmp[ord]
+
+    comm.lines <- lapply(comms, function(x) {
+        if ("end" %in% names(x)) {
+            with(x, sprintf("\\ind %d--%s.  %s, %s.\n", start, end, role, entity))
+        } else {
+            with(x, sprintf("\\ind %d.  %s, %s.\n", start, role, entity))
+        }
+    })
+
+    ## Then grant reviews
+    grants <- paste0("\\ind Research proposal reviewer for ",
+                     paste0(l[[2]], collapse=", "), "\n")
+
+    ## Then journal reviews
+    journals <- paste0("\\ind Journal reviewer for ",
+                       paste0(paste("\\emph{", l[[3]], "}", sep=""), collapse=", "), "\n")
+
+    journals <- str_replace(journals, "&", "\\\\&")
+
+    ## Then consultancy
+    consultancy <- paste0("\\ind Independent consultancy for ",
+                          paste0(l[[4]], collapse=", "), "\n")
+
+    lines <- c(comm.lines, grants, journals, consultancy)
+    return(lines)
+}
+
+process_yaml("../content/service.yaml",
+             "../templates/service.tex", f)
