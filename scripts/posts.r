878c59c39cd8fc82fc8cf5c37634ef3bdd086be6
James Keirstead <james.keirstead@gmail.com>
2015-02-27 19:36:36 +0000

    Converted the posts

--- scripts/posts.r
+++ scripts/posts.r
@@ -1,26 +1,26 @@
-## Test for parsing YAML
+#' Process the posts
+source("yaml.r")
 
-library(yaml)
+f <- function(l) {
+    tmp <- l[[1]]
 
-## Load the talks
-talks <- yaml.load_file("../content/posts.yaml")[[1]]
+    lines <- lapply(tmp, function(x) {
+        topline <- sprintf("\\emph{%s}\n", x$employer)
 
-lines <- lapply(talks, function(x) {
-    topline <- sprintf("\\emph{%s}\n", x$employer)
-
-    roles <- lapply(x$roles, function(x) {
-        if ("end" %in% names(x)) {
-            with(x, sprintf("\\ind %s--%s.  %s.\n", start, end, title))
-        } else {
-            with(x, sprintf("\\ind %s.  %s.\n", start, title))
-        }
+        roles <- lapply(x$roles, function(x) {
+            if ("end" %in% names(x)) {
+                with(x, sprintf("\\ind %s--%s.  %s.\n", start, end, title))
+            } else {
+                with(x, sprintf("\\ind %s.  %s.\n", start, title))
+            }
+        })
+        
+        c(list(topline), roles, list("\\medskip\n"))
     })
+    
+    lines <- head(unlist(lines), -1)
+    return(lines)
+}
 
-    c(list(topline), roles, list("\\medskip\n"))
-})
-
-lines <- head(unlist(lines), -1)
-
-out.file <- "../templates/posts.tex"
-if (file.exists(out.file)) file.remove(out.file)
-invisible(lapply(lines, write, file=out.file, append=TRUE))
+process_yaml("../content/posts.yaml",
+             "../templates/posts.tex", f)
