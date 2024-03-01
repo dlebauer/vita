fa798db5aab33a69ba14007f3a8b38dd67692434
David LeBauer <dlebauer@gmail.com>
2024-02-29 20:26:05 -0700

    add CCT protocols citation, changed address and website

--- write_cv.R
+++ write_cv.R
@@ -8,4 +8,4 @@ myCV.sty <- system.file("style/dl-vita.sty", package = "vita")
 generate_cv(content = myCV.yaml,
             style = myCV.sty,
             outdir = "outdir")
-
+file.copy("outdir/dlebauer-full-vita.pdf", "dlebauer-full-vita.pdf", overwrite = TRUE)
